60f358148c782ca5f015d6a118d2f2ab10834e35
plngetich <plngetich@kabarak.ac.ke>
2022-04-20 14:59:39 +0300

    Initial Commit

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Task Tracker</h1>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+
+}
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
